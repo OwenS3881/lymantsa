b366c78062a8c39ed648926d45428543609e6005
OwenS3881 <owenski08@gmail.com>
2022-07-05 20:08:19 -0400

    added a spot for name when joining, added location of school on home page

--- join/join.js
+++ join/join.js
@@ -11,8 +11,8 @@ window.onload = function()
         const params =
         {
             type: "Join",
-            field_1: document.querySelector(".email-input").value,
-            field_2: "N/A",
+            field_1: "ID: " + document.querySelector(".email-input").value,
+            field_2: "Name: " + document.querySelector(".join-name-input").value,
             field_3: "N/A"
         }
         emailjs.send("service_wmvvg3l","template_blht2yf", params).then(function (res)
