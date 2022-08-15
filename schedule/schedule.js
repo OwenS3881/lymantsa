a6b635061d2c56ff0078185d0f09dc2e2871ccd8
OwenS3881 <owenski08@gmail.com>
2022-08-15 17:46:37 -0400

    improvements to schedule system

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -2,32 +2,34 @@
 
 class Event
 {
-    constructor(name, date, location, description)
+    constructor(name, date, location, description, type)
     {
         this.name = name;
         this.date = date;
         this.location = location;
         this.description = description;
+        this.type = type;
     }
 
     getName() { return this.name; }
     getDate() { return this.date; }
     getLocation() { return this.location; }
     getDescription() { return this.description; }
+    getType() { return this.type; }
 }
 
 //Add new events here
 const events =
 [
-    new Event("First Meeting", new Date(2022, 7, 22), "Ms. Acken's Room, 06-039", "<strong>Highly recommended for all members.</strong> We will be going over important information regarding TSA including what TSA is, for new members, and some important changes for all members"),
+    new Event("First Meeting", new Date(2022, 7, 22), "Ms. Acken's Room, 06-039", "<strong>Highly recommended for all members.</strong> We will be going over important information regarding TSA including what TSA is, for new members, and some important changes for all members", "IMPORTANT"),
 
-    new Event("Leadership Conference", new Date(2022, 9, 19), "Altamonte Springs Hilton", "Open to all members - not just leadership. <strong>Leadership members required to attend.</strong> NOTE: if you are a member of leadership, confirmation of your ability to attend will be required prior to the first TSA meeting of the 2022-2023 school year. If unable to attend, the position may potentially be filled by a TSA member/candidate who is able to attend and will then fill the position for the school year. The event starts on the 19th and ends on the 22nd"),
+    new Event("Leadership Conference", new Date(2022, 9, 19), "Altamonte Springs Hilton", "Open to all members - not just leadership. <strong>Leadership members required to attend.</strong> NOTE: if you are a member of leadership, confirmation of your ability to attend will be required prior to the first TSA meeting of the 2022-2023 school year. If unable to attend, the position may potentially be filled by a TSA member/candidate who is able to attend and will then fill the position for the school year. The event starts on the 19th and ends on the 22nd", "OTHER"),
 
-    new Event("Regional Competition", new Date(2022, 11, 3), "Palm Bay High School", "One day event - not all competitions are included - all members are highly encouraged to attend, however, this is not a requirement to be a member of the TSA club"),
+    new Event("Regional Competition", new Date(2022, 11, 3), "Palm Bay High School", "One day event - not all competitions are included - all members are highly encouraged to attend, however, this is not a requirement to be a member of the TSA club", "OTHER"),
 
-    new Event("2023 State Conference and Competition", new Date(2023, 1, 22), "Orlando Universal DoubleTree", "<strong>Attendance to to this 3 day event is required as a member of TSA</strong> - all members will be required to compete in a minimum of 2 events and a maximum of 6. The event starts on the 22nd and ends on the 25th"),
+    new Event("2023 State Conference and Competition", new Date(2023, 1, 22), "Orlando Universal DoubleTree", "<strong>Attendance to to this 3 day event is required as a member of TSA</strong> - all members will be required to compete in a minimum of 2 events and a maximum of 6. The event starts on the 22nd and ends on the 25th", "OTHER"),
 
-    new Event("2023 National TSA Conference", new Date(2023, 5, 28), "Louisville, Kentucky", "Attendance is optional. This event starts on June 28th and ends on July 2nd"),
+    new Event("2023 National TSA Conference", new Date(2023, 5, 28), "Louisville, Kentucky", "Attendance is optional. This event starts on June 28th and ends on July 2nd", "OTHER"),
 ];
 //-------------------------------------------------
 
@@ -52,7 +54,9 @@ events.sort(function (first, second)
 });
 
 const templateEvent = document.querySelector(".event-box");
-const container = document.querySelector(".schedule-content-container");
+
+const upcomingContainer = document.querySelector("#upcoming-events-container");
+const pastContainer = document.querySelector("#past-events-container");
 
 for (const event of events) 
 {
@@ -61,20 +65,50 @@ for (const event of events)
     if (typeof date != "object") 
     {
         continue;
-    }
+    } 
+
+    const newEvent = templateEvent.cloneNode(true);
 
     if (date.getTime() + 86400000 < Date.now())
     {
-        continue;
+        pastContainer.appendChild(newEvent);
     }
+    else
+    {
+        upcomingContainer.appendChild(newEvent);
 
-    const newEvent = templateEvent.cloneNode(true);
-    container.appendChild(newEvent);
+        if (event.getType() == "IMPORTANT")
+        {
+            newEvent.querySelector(".event-name").classList.add("important");
+        }
+    }
 
     newEvent.querySelector(".event-name").innerHTML = event.getName();
     newEvent.querySelector(".event-date").innerHTML = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
     newEvent.querySelector(".event-location").innerHTML = event.getLocation();
-    newEvent.querySelector(".event-description").innerHTML = event.getDescription();
+    newEvent.querySelector(".event-description").innerHTML = event.getDescription();   
 }
 
 templateEvent.remove();
+
+//Time switch
+const upcomingButton = document.querySelector("#schedule-time-upcoming-button");
+const pastButton = document.querySelector("#schedule-time-past-button");
+
+upcomingButton.addEventListener("click", function()
+{
+    upcomingButton.classList.add("selected");
+    pastButton.classList.remove("selected");
+
+    upcomingContainer.classList.remove("hidden");
+    pastContainer.classList.add("hidden");
+});
+
+pastButton.addEventListener("click", function()
+{
+    pastButton.classList.add("selected");
+    upcomingButton.classList.remove("selected");
+
+    pastContainer.classList.remove("hidden");
+    upcomingContainer.classList.add("hidden");
+});
