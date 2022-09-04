b25965f3d632c1298099fca4562b872517c3eb8f
OwenS3881 <owenski08@gmail.com>
2022-09-04 18:54:33 -0400

    reveal doesnt auto reveal hidden items

--- reveal.js
+++ reveal.js
@@ -12,6 +12,11 @@ function reveal(className)
 
     for (let i = 0; i < reveals.length; i++)
     {
+        if (window.getComputedStyle(reveals[i]).display === "none")
+        {
+            continue;
+        }
+
         const windowHeight = window.innerHeight;
         const elementTop = reveals[i].getBoundingClientRect().top;
         const elementVisible = 150;
