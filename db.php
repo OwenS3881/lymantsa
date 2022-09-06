55a9129f7c57e7b208649a7f3c7dbb99093e8a2f
OwenS3881 <owenski08@gmail.com>
2022-09-06 17:30:40 -0400

    ?

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+
+//connect to database
+$conn = mysqli_connect("box5893.bluehost.com", "szymansk_tsa_app", "jd(&hdj(YGe(HG*9d(", "szymansk_lymantsa");
+
+?>
--- join/join.php
+++ join/join.php
@@ -1,7 +1,6 @@
 <?php
 
-//connect to database
-$conn = mysqli_connect("box5893.bluehost.com", "szymansk_tsa_app", "jd(&hdj(YGe(HG*9d(", "szymansk_lymantsa");
+include_once '../db.php';
 
 // Check connection
 if (!$conn) 
