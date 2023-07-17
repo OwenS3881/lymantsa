e696d0cc91bee05b00109029ef9135b63e2ba3e4
OwenS3881 <owenski08@gmail.com>
2023-07-17 16:02:00 -0400

    more officers page

--- officers/officers.js
+++ officers/officers.js
@@ -1,10 +1,15 @@
+document.querySelector(".officers-hidden-wrapper").classList.add("hidden");
+
 window.onload = function()
 {
     document.querySelector(".officer-login-form").addEventListener("submit", function(event)
     {      
-        //event.preventDefault();
+        event.preventDefault();
         
-        checkPassword();
+        if (checkPassword())
+        {
+            document.querySelector(".officers-hidden-wrapper").classList.add("hidden");
+        }
     });
 }
 
@@ -12,6 +17,17 @@ function checkPassword()
 {
     const enteredPassword = document.querySelector(".officers-password-input").value;
 
+    console.log(enteredPassword);
+
     const request = new XMLHttpRequest();
-    request.open("POST", "../checkpassword.php")
+    request.open("POST", "../checkpassword.php");
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.onreadystatechange = function()
+    {
+        if (request.readyState == 4 && request.status == 200)
+        {
+            return request.responseText == "True";      
+        }
+    }
+    request.send("password=" + enteredPassword);
 }
